d6c368dacb5e3665eea3388648d372f5f1af4fbf
slaye <uduterrage@gmail.com>
2025-08-19 15:13:17 +0200

    feat: working predict.py

--- sources/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 42-ft-linear-regression!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ sources/predict.py
@@ -0,0 +1,38 @@
+import sys
+
+def get_training_output() -> tuple[float, float]:
+	"""
+	Temporary docstring.
+	"""
+	try:
+		with open("training.out", "r", encoding="utf-8") as file:
+			lines: list[str] = file.readlines()
+			slope: float = float(lines[0])
+			intersect: float = float(lines[1])
+			return (slope, intersect)
+	except FileNotFoundError:
+		return (0, 0)
+	except Exception as error:
+		print("Error:", error, file=sys.stderr)
+		sys.exit(1)
+
+def main():
+	"""
+	Temporary docstring.
+	"""
+	try:
+		function: tuple[float, float] = get_training_output()
+		mileage: int = int(input("Car mileage: "))
+
+		if mileage < 0:
+			raise ValueError("the mileage can't be negative")
+		result: float = function[0] * mileage + function[1]
+		result = 0 if result < 0 else result
+		print(f"The estimated price is: {result:.2f}$")
+	except Exception as error:
+		print("Error:", error, file=sys.stderr)
+		sys.exit(1)
+
+
+if __name__ == "__main__":
+	main()
