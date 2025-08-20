6e38ea098108a9e8805d06e047f78db9155c68cc
slaye <uduterrage@gmail.com>
2025-08-20 12:59:02 +0200

    feat: working model training

--- sources/train.py
+++ sources/train.py
@@ -1,33 +1,68 @@
 import sys
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-DATA_PATH = "includes/data.csv"
-MAX_DEPTH = 10
-LERNING_RATE = 0.01
+DATASET_PATH = "includes/data.csv"
+MAX_DEPTH = 1000
+MIN_STEP = 0.0001
+LEARNING_RATE = 0.1
 
-def compute_t0(data: pd.DataFrame, t0: float, t1: float) -> float:
+def normalize_dataset(dataset: pd.DataFrame) -> None:
 	"""
 	Temporary docstring.
 	"""
-	data_length: int = data.shape[0]
-	derivated_mse_sum: float = sum([
-		t1 * x["km"] + t0 - x["price"]
-		for _, x in data.iterrows()
-	])
-	derivated_mse: float = 1 / data_length * derivated_mse_sum
-	return LERNING_RATE * derivated_mse
+	for column in dataset.columns:
+		values = dataset[column]
+		dataset[column] = (values - values.min()) / (values.max() - values.min())
 
-def compute_t1(data: pd.DataFrame, t0: float, t1: float) -> float:
+def compute_t0_step(dataset: pd.DataFrame, t0: float, t1: float) -> float:
 	"""
 	Temporary docstring.
 	"""
-	data_length: int = data.shape[0]
+	data_length: int = dataset.shape[0]
 	derivated_mse_sum: float = sum([
-		(t1 * x["km"] + t0 - x["price"]) * x["km"]
-		for _, x in data.iterrows()
+		(t1 * x["km"] + t0) - x["price"]
+		for _, x in dataset.iterrows()
 	])
 	derivated_mse: float = 1 / data_length * derivated_mse_sum
-	return LERNING_RATE * derivated_mse
+	return LEARNING_RATE * derivated_mse
+
+def compute_t1_step(dataset: pd.DataFrame, t0: float, t1: float) -> float:
+	"""
+	Temporary docstring.
+	"""
+	data_length: int = dataset.shape[0]
+	derivated_mse_sum: float = sum([
+		((t1 * x["km"] + t0) - x["price"]) * x["km"]
+		for _, x in dataset.iterrows()
+	])
+	derivated_mse: float = 1 / data_length * derivated_mse_sum
+	return LEARNING_RATE * derivated_mse
+
+def reverse_normalisation(dataset: pd.DataFrame, dataset_min: list[float], dataset_max: list[float]) -> None:
+	"""
+	Temporary docstring.
+	"""
+	for index, column in enumerate(dataset.columns):
+		values = dataset[column]
+		dataset[column] = values * (dataset_max[index] - dataset_min[index]) + dataset_min[index]
+
+def plot_model(dataset: pd.DataFrame, t0: float, t1: float) -> None:
+	"""
+	Temporary docstring.
+	"""
+	sns.set_theme()
+	sns.scatterplot(
+		data=dataset,
+		x="km",
+		y="price"
+	)
+	x_line = np.linspace(min(dataset["km"]), max(dataset["km"]), 10)
+	y_line = t1 * x_line + t0
+	plt.plot(x_line, y_line)
+	plt.show()
 
 def main():
 	"""
@@ -36,12 +71,21 @@ def main():
 	try:
 		t0: float = 0
 		t1: float = 0
-		data = pd.read_csv(DATA_PATH)
+		dataset: pd.DataFrame = pd.read_csv(DATASET_PATH)
+		dataset_min: list[float] = [dataset[x].min() for x in dataset.columns]
+		dataset_max: list[float] = [dataset[x].max() for x in dataset.columns]
+		normalize_dataset(dataset)
 		for _ in range(MAX_DEPTH):
-			t0_holder = compute_t0(data, t0, t1)
-			t1_holder = compute_t1(data, t0, t1)
-			t0 = t0_holder
-			t1 = t1_holder
+			t0_step = compute_t0_step(dataset, t0, t1)
+			t1_step = compute_t1_step(dataset, t0, t1)
+			if abs(t0_step) <= MIN_STEP and abs(t1_step) <= MIN_STEP:
+				break
+			t0 -= t0_step
+			t1 -= t1_step
+		t1_holder = (t1 * (dataset_max[1] - dataset_min[1])) / (dataset_max[0] - dataset_min[0])
+		t0_holder = t0 * (dataset_max[1] - dataset_min[1]) + dataset_min[1] - t1_holder * dataset_min[0]
+		t0, t1 = t0_holder, t1_holder
+		# TODO: write output to training.out
 		print(f"{t1:.2f}x + {t0:.2f}")
 	except Exception as error:
 		print("Error:", error, file=sys.stderr)
